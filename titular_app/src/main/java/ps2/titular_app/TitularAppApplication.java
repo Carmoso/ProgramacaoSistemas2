e99b51d36fa76c7b2b8a85cd57510c906bcfb3c0
Carmoso <eduardo.carmo@mackenzista.com.br>
2025-09-20 01:33:06 +0000

    Finalizar lab

---
+++ titular_app/src/main/java/ps2/titular_app/TitularAppApplication.java
@@ -0,0 +1,90 @@
+package ps2.titular_app;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import static ps2.titular_app.ES.input;
+import static ps2.titular_app.ES.print;
+
+import java.util.Scanner;
+
+@SpringBootApplication
+public class TitularAppApplication implements CommandLineRunner {
+
+	@Autowired
+	private TitularRepo titularrepo;
+
+	public static void main(String[] args) {
+		SpringApplication.run(TitularAppApplication.class, args);
+	}
+
+	public void criar() {
+		Titular t;
+		t = new Titular();
+		t.setNome("Thiago");
+		t.setCpf("333.333.333-3");
+		titularrepo.save(t);
+		System.out.println("Titular criado com o id " + t.getId());
+	}
+
+	public void lerTudo() {
+		Iterable<Titular> titulares = titularrepo.findAll();
+		for (Titular t : titulares) {
+			System.out.println(t);
+		}
+	}
+
+	public void buscarPeloNum() {
+		Iterable<Titular> titulares = titularrepo.findAll();
+		Scanner sc = new Scanner(System.in);
+		long id = sc.nextLong();
+		for (Titular t : titulares) {
+			if (t.getId() = id) {
+				System.out.println(t);
+			}
+		}
+
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("# GERENCIADOR DE TITULARES!");
+		boolean sair = false;
+		String menu = "\n(1) Mostrar todos os titulares";
+		menu += "\n(2) Buscar um titular específico pelo número";
+		menu += "\n(3) Criar um novo titular";
+		menu += "\n(4) Alterar os dados do titular";
+		menu += "\n(5) Apagar um titular";
+		menu += "\n(0) Sair \n";
+		menu += "Escolha uma opção: ";
+
+		while (!sair) {
+			String op = input(menu);
+			switch (op) {
+				case "5":
+					lerTudo();
+					break;
+				case "4":
+					lerTudo();
+					break;
+				case "3":
+					lerTudo();
+					break;
+				case "2":
+					lerTudo();
+					break;
+				case "1":
+					lerTudo();
+					break;
+				case "0":
+					sair = true;
+					break;
+				default:
+					print("Opção inválida!");
+			}
+		}
+	}
+
+}
